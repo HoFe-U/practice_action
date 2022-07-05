0f56f54576ecd7f57290e8e7479586d1992f747f
HoFe-U <hochul-y@naver.com>
2022-07-05 17:17:13 +0900

    Initial commit

---
+++ src/main/java/com/example/practice_action/PracticeActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeActionApplication.class, args);
+    }
+
+}

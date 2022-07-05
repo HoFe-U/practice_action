2ee678b3229c0d7f6dc0359e66208cbb9b206a99
HoFe-U <hochul-y@naver.com>
2022-07-05 19:18:24 +0900

    push-test

--- src/main/java/com/example/practice_action/Hello.java
+++ src/main/java/com/example/practice_action/Hello.java
@@ -2,7 +2,7 @@ package com.example.practice_action;
 
 public class Hello {
     private String hello;
-    private String aaa;
+    private String aaaa;
 
     public Hello(String hello) {
         this.hello = hello;
